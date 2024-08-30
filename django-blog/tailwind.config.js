088d803b8ab9a57571535f36f9cb7080655ee2af
AnzaTamveel <anzatamveel@gmail.com>
2024-08-30 12:54:04 +0500

    Django Blog Application

---
+++ django-blog/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './blog/templates/**/*.html',
+    './template/**/*.html',
+    './static/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+
+  ],
+}
+
