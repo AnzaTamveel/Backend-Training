23bd53ef90db274ff74fc983869ca4b9f027149d
AnzaTamveel <anzatamveel@gmail.com>
2024-09-07 22:27:28 +0500

    Tamveel's Blog

--- django-blog/blog/views.py
+++ django-blog/blog/views.py
@@ -1,12 +1,23 @@
-from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from blog.models import Post, Comment
+from blog.models import Post, Comment, Category
 from blog.forms import CommentForm
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+
+
+def base_view(request):
+    categories = Category.objects.all()
+    context = {
+        "categories": categories,
+    }
+    return render(request, "base.html", context)
 
 def blog_index(request):
     posts = Post.objects.all().order_by("-created_on")
+    categories = Category.objects.all()
     context = {
         "posts": posts,
+        "categories": categories,
     }
     return render(request, "blog/index.html", context)
 
@@ -14,30 +25,36 @@ def blog_category(request, category):
     posts = Post.objects.filter(
         categories__name__contains=category
     ).order_by("-created_on")
+    categories = Category.objects.all()
     context = {
         "category": category,
         "posts": posts,
+        "categories": categories,
     }
     return render(request, "blog/category.html", context)
 
+
 def blog_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    comments = Comment.objects.filter(post=post)
     form = CommentForm()
+
     if request.method == "POST":
         form = CommentForm(request.POST)
         if form.is_valid():
-            comment = Comment(
-                author=form.cleaned_data["author"],
-                body=form.cleaned_data["body"],
-                post=post,
-            )
+            comment = form.save(commit=False)
+            comment.post = post
             comment.save()
             return HttpResponseRedirect(request.path_info)
 
-    comments = Comment.objects.filter(post=post)
+    similar_category_posts = Post.objects.filter(categories__in=post.categories.all()).exclude(pk=post.pk).distinct()
+
+    categories = Category.objects.all()
     context = {
         "post": post,
         "comments": comments,
-        "form": CommentForm(),
+        "form": form,
+        "categories": categories,
+        "similar_category_posts": similar_category_posts,
     }
     return render(request, "blog/detail.html", context)
